14cde663d0fcf1ee2e2feaf94fae800dfcfe4035
Piotr Archi <piotr.borysowski@gmail.com>
2024-05-01 13:43:31 +0200

    Add nav barwith logo

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,3 @@
+import { Inter, Work_Sans } from "next/font/google";
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Work_Sans({ subsets: ["latin"] });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,23 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                main: "#F4F8FA",
+                "midnight-purple": "#423C66",
+            },
+        },
+        screens: {
+            mobile: "360px",
+            // => @media (min-width: 360px) { ... }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
